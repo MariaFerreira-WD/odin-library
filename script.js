a906aa99aa8d2f294f266777bfcb33c87753a93d
Maria Ferreira <261775209+MariaFerreira-WD@users.noreply.github.com>
2026-03-17 16:08:52 +0000

    function that loops through the array and displays each book on the page

--- script.js
+++ script.js
@@ -13,9 +13,39 @@ function Book(title, author, pages, read) {
   };
 }
 
-function addBookToLibrary(title, author, pages, read) {
-  const book = new Book(title, author, pages, read);
-  myLibrary.push(book);
+function displayBook() {
+  let text = "";
+  for (let index = 0; index < myLibrary.length; index++) {
+    text += `${myLibrary[index].title} 
+             ${myLibrary[index].author}, 
+             ${myLibrary[index].pages} pages <br>`;
+  }
+
+  document.getElementById("library").innerHTML = text;
 }
 
-addBookToLibrary("The Hobbit", "by J.R.R Tolkien", 295, false);
+function addBookToLibrary() {
+  event.preventDefault();
+
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.getElementById("read").checked;
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+  displayBook();
+}
+
+/* addBookToLibrary("The Hobbit", "by J.R.R Tolkien", 295, false);
+addBookToLibrary(
+  "The Priory of the Orange Tree",
+  "Samantha Shannon",
+  848,
+  true,
+);
+addBookToLibrary(
+  "The Seven Husbands of Evelyn Hugo",
+  "Taylor Jenkins Reid",
+  389,
+  true,
+); */
